17ee24407985476411a7d93b44a9951567bc3bea
williamtjjd4 <72386097+williamtjjd4@users.noreply.github.com>
2020-11-02 22:35:17 +0800

    js

--- w08/script.js
+++ w08/script.js
@@ -1,4 +1,5 @@
 /* 抓到按鈕 */
-document.querySelector('.main-btn').addEventListener('click',() => {
+document.querySelector('.menu-btn').addEventListener('click',() => {
     document.querySelector('.main-menu').classList.toggle('show');
-});
+}
+);
